f59d39ad378cdf706f6e90bdcd692e1aeb64dee3
Oleksandr Smakhtin <skiper.smaxtin@icloud.com>
2022-09-25 18:00:52 +0300

    Add IBActions

--- iPhone Calculator/ViewController.swift
+++ iPhone Calculator/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var displayLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func culcBtnPressed(_ sender: UIButton) {
+        print("culcButton pressed")
+        if let act = sender.titleLabel?.text {
+            if act == "AC" {
+                displayLbl.text = "0"
+            }
+        }
+    }
+    
+        
+    @IBAction func numBtnPressed(_ sender: UIButton) {
+        if let num = sender.titleLabel?.text {
+            //displayLbl.text = ""
+            
+            displayLbl.text! += num
+            if displayLbl.text?.first == "0" {
+                displayLbl.text?.removeFirst()
+            }
+        }
+    }
+    
+
+    
 
 }
 
