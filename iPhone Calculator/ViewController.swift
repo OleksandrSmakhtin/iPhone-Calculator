c04c273ab9462b1ea430bea84a0d38ad2a48e5de
Oleksandr Smakhtin <skiper.smaxtin@icloud.com>
2022-09-27 20:26:36 +0300

    Add tap view effect and tap sound effect

--- iPhone Calculator/ViewController.swift
+++ iPhone Calculator/ViewController.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     
     
     @IBOutlet weak var displayLbl: UILabel!
+    
+    var audioPlayer = AVAudioPlayer()
+    var isFinishedTypingNumber = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,25 +25,90 @@ class ViewController: UIViewController {
     
     @IBAction func culcBtnPressed(_ sender: UIButton) {
         print("culcButton pressed")
+        isFinishedTypingNumber = true
+        
         if let act = sender.titleLabel?.text {
+            
             if act == "AC" {
                 displayLbl.text = "0"
+                
             }
+            
+            
+            
+            
+            
         }
     }
     
-        
     @IBAction func numBtnPressed(_ sender: UIButton) {
         if let num = sender.titleLabel?.text {
-            //displayLbl.text = ""
             
-            displayLbl.text! += num
-            if displayLbl.text?.first == "0" {
-                displayLbl.text?.removeFirst()
+            tapSound()
+            
+            // touch view action
+            if let superView = sender.superview {
+                superView.backgroundColor = .gray
             }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.13, execute: {
+                if let superView = sender.superview {
+                    superView.backgroundColor = #colorLiteral(red: 0.1951842308, green: 0.200163126, blue: 0.195766449, alpha: 1)
+                }
+            })
+            
+            
+            if displayLbl.text?.count == 9 {
+                return
+            }
+            
+            
+            
+//            // set max char
+//            if displayLbl.text?.count == 11 {
+//                return
+//            }
+//            if displayLbl.text!.count >= 4 {
+//                // make spacing
+//                switch displayLbl.text?.count {
+//                case 4:
+//                    displayLbl.text?.insert(" ", at: displayLbl.text.index)
+//                case 7:
+//                    displayLbl.text?.append(" ")
+//                case 2:
+//                    displayLbl.text?.append(" ")
+//                default:
+//                    print("bruh")
+//                }
+//            }
+            
+            
+            
+            
+            // print numbers
+            if isFinishedTypingNumber {
+                displayLbl.text = num
+                isFinishedTypingNumber = false
+            } else {
+                displayLbl.text?.append(num)
+            }
+            
+            
         }
     }
     
+    
+    
+    func tapSound() {
+        let pathToSound = Bundle.main.path(forResource: "tap", ofType: "mp3")!
+        let url = URL(filePath: pathToSound)
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer.play()
+        } catch {
+            print("Audio error")
+        }
+    }
 
     
 
